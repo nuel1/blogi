60a8bbb7ed9afe56b35d381487e579d7b6366375
joseph <nwekejoseph52@gmail.com>
2024-01-10 03:11:39 -0800

    set route scroll position cofig to "top"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      scrollPositionRestoration: 'enabled',
+      scrollPositionRestoration: 'top',
     }),
   ],
   exports: [RouterModule],
